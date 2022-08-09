441727d318a473f0e8c1a69624ba88e6ecebb2c9
Justin Liu <justinwl2097@gmail.com>
2022-08-09 18:32:06 -0500

    getCoords removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,44 +11,41 @@ var buttonClickHandler = function (event) {
 
     if (city) {
         console.log("Yeah, " + city + " probably exists.");
-        getCityCoords(city);
+        //getCityCoords(city);
         //get forecast of city afterwards
+        getForecast(city);
     }
     else {
         console.log("Input shouldn't be blank.");
     }
 }
 
-var getCityCoords = function (city) {
+var getForecast = function(cityName) {
+    console.log("TODOLED");
 
-    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=89a4f3c910149accc122e8310674f685";
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=89a4f3c910149accc122e8310674f685";
+    //"http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat + "&lon=" + lon + "&cnt=6&appid=89a4f3c910149accc122e8310674f685";
 
     fetch(apiUrl)
         .then(function (res) {
             if (res.ok) {
                 res.json().then(function (data) {
                     console.log(data);
-                    if (data.length === 0) {
-                        return;
+
+                    for (var i = 0; i < data.list.length; i += 8) {
+                        console.log(data.list[i]);
                     }
-                    console.log("Latitude: " + data[0].lat)
-                    console.log("Longitude: " + data[0].lon)
-                    // then actually look up the weather
-                    // although it's very likely to have multiple cities with the same name, just use the first element in the data returned
-                    // getForecast(lat, long)
-                });
+
+                    // send this to a function that adds these forecasts to the HTML
+                })
             }
             else {
                 console.log("Failed with a status code of " + res.statusText);
             }
         })
         .catch(function (error) {
-            console.log("Cannot connect to Geocoding API")
+            console.log("Cannot connect to weather API")
         })
-}
-
-var getForecast = function(lati, long) {
-    console.log
 }
 
 userFormEl.addEventListener("submit", buttonClickHandler);
