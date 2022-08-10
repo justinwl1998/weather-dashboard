ab4ab2be07020b86a0998876eea193581177feea
Justin Liu <justinwl2097@gmail.com>
2022-08-09 19:59:48 -0500

    added two functions to call to get current weather and a five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ var buttonClickHandler = function (event) {
         console.log("Yeah, " + city + " probably exists.");
         //getCityCoords(city);
         //get forecast of city afterwards
+        getCurrentWeather(city);
         getForecast(city);
     }
     else {
@@ -20,23 +21,48 @@ var buttonClickHandler = function (event) {
     }
 }
 
+var getCurrentWeather = function(cityName) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=89a4f3c910149accc122e8310674f685"
+
+    fetch(apiUrl)
+        .then(function(res) {
+            if (res.ok) {
+                res.json().then(function(data) {
+                    console.log("Today's weather:");
+                    console.log(data)
+                    console.log("End of getCurrentWeather")
+
+                    // function to modify the current weather section
+                })
+            }
+            else {
+                console.log("Failed with a status code of " + res.statusText);
+            }
+        })
+        .catch(function (error) {
+            console.log("Cannot connext to weather API");
+        })
+}
+
 var getForecast = function(cityName) {
     console.log("TODOLED");
 
-    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=89a4f3c910149accc122e8310674f685";
-    //"http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat + "&lon=" + lon + "&cnt=6&appid=89a4f3c910149accc122e8310674f685";
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=89a4f3c910149accc122e8310674f685"
 
     fetch(apiUrl)
         .then(function (res) {
             if (res.ok) {
                 res.json().then(function (data) {
+                    //console.log("This should execute and finish last.")
                     console.log(data);
 
                     for (var i = 0; i < data.list.length; i += 8) {
                         console.log(data.list[i]);
+                        console.log("Date for this forecast: " + data.list[i].dt_txt);
                     }
 
                     // send this to a function that adds these forecasts to the HTML
+                    console.log("End of getForecast")
                 })
             }
             else {
